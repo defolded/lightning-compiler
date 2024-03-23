4443758f9be8f12c8459d54e092edc4cc9d8943d
defolded <hailrake04@gmail.com>
2024-03-22 22:57:28 -0400

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+int main(int argc, char* argv[]) {
+    if (argc != 2)
+        return EXIT_FAILURE;
+
+    std::string contents{};
+    {
+        std::stringstream contents_stream;
+        std::fstream input(argv[1], std::ios::in);
+        contents_stream << input.rdbuf();
+        contents = contents_stream.str();
+    }
+
+    std::cout << contents << std::endl;
+
+    return EXIT_SUCCESS;
+}
